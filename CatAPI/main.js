9cd468134e7d8585818156184fd0b0a395987368
JeisonPC <jeida16@yahoo.es>
2022-08-09 18:18:54 -0500

    Añadiendo cambios relacionados con los query Parameters: Class 4

--- CatAPI/main.js
+++ CatAPI/main.js
@@ -1,24 +1,41 @@
 console.log('Hello, World');
 
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thecatapi.com/v1/images/search?limit=3';
 
-const cImg = (link) => {
-    fetch(link)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img');
-        img.src = data[0].url;
-    });
-};
+const loadImage = async () => {
+    const res = await fetch(URL);
+    const data = await res.json();
 
-const recargar = async () => {
-    const URL2 = 'https://api.thecatapi.com/v1/images/search';
-
-    try{
-        await cImg(URL2);    
-    } catch(error){
-        console.error(error)
-    }
+    console.log(data);
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
-cImg(URL);
+
+// const cImg = (link) => {
+//     fetch(link)
+//     .then(res => res.json())
+//     .then(data => {
+//         const img = document.querySelector('img');
+//         img.src = data[0].url;
+//     });
+// };
+
+// const recargar = async () => {
+//     const URL2 = 'https://api.thecatapi.com/v1/images/search';
+
+//     try{
+//         await cImg(URL2);    
+//     } catch(error){
+//         console.error(error)
+//     }
+// }
+
+// cImg(URL);
+
+loadImage();
