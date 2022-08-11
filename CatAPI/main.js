7bbc8e0ca99f5001d25c283bcdd06dd4c6ec72b0
JeisonPC <jeida16@yahoo.es>
2022-08-10 19:46:33 -0500

    Añadiendo configuración de errores en las funciones :Class9

--- CatAPI/main.js
+++ CatAPI/main.js
@@ -1,43 +1,55 @@
-console.log('Hello, World');
 
-const URL = 'https://api.thecatapi.com/v1/images/search?limit=4&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
 
-const loadImage = async () => {
-    const res = await fetch(URL);
+const API_URl_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
+const API_URl_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
+const API_ERROR_MICHIS = 'https://http.cat/'
+
+const spanError = document.getElementById("Error");
+
+const loadRandomMichis = async () => {
+    const res = await fetch(API_URl_RANDOM);
     const data = await res.json();
 
+    console.log('Random');
     console.log(data);
+
+    if (res.status !== 200) {
+        
+        spanError.innerHTML = `"Hubo un error: Error_${res.status},${data.message}`;
+        spanError.insertAdjacentHTML(
+            "beforeend",
+            `<img src =${API_ERROR_MICHIS}${res.status} id="img-error">`
+        );
+    }else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    const img4 = document.getElementById('img4');
     
     img1.src = data[0].url;
     img2.src = data[1].url;
-    img3.src = data[2].url;
-    img4.src = data[3].url;
-}
+
+    }
+};
+
+const loadFavoritesMichis = async () => {
+    const res = await fetch(API_URl_FAVORITES);
+    const data = await res.json();
+
+    console.log('Favorites');
+    console.log(data);
+
+    if (res.status !== 200) {
+        
+        spanError.innerHTML = `"Hubo un error: Error_${res.status},${data.message}`;
+        spanError.insertAdjacentHTML(
+            "beforeend",
+            `<img src =${API_ERROR_MICHIS}${res.status} id="img-error">`
+        );
+    }else {
+        
+    }
+   
+};
 
 
-// const cImg = (link) => {
-//     fetch(link)
-//     .then(res => res.json())
-//     .then(data => {
-//         const img = document.querySelector('img');
-//         img.src = data[0].url;
-//     });
-// };
-
-// const recargar = async () => {
-//     const URL2 = 'https://api.thecatapi.com/v1/images/search';
-
-//     try{
-//         await cImg(URL2);    
-//     } catch(error){
-//         console.error(error)
-//     }
-// }
-
-// cImg(URL);
-
-loadImage();
+loadRandomMichis();
+loadFavoritesMichis();
