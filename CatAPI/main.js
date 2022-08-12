b3a4d6c24c026460f27907b72a9f297bc2beac7d
JeisonPC <jeida16@yahoo.es>
2022-08-12 17:15:16 -0500

    Añadiendo la funcion de michis favoritos funcional: Class 10

--- CatAPI/main.js
+++ CatAPI/main.js
@@ -1,7 +1,7 @@
 
 
-const API_URl_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
-const API_URl_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
+const API_URl_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const API_URl_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=0e67e603-210b-4a02-9582-778b82a47c62';
 const API_ERROR_MICHIS = 'https://http.cat/'
 
 const spanError = document.getElementById("Error");
@@ -23,18 +23,18 @@ const loadRandomMichis = async () => {
     }else {
     const img1 = document.getElementById('img1');
     const img2 = document.getElementById('img2');
-    
+     
     img1.src = data[0].url;
     img2.src = data[1].url;
 
     }
 };
 
-const loadFavoritesMichis = async () => {
+const loadFavouriteMichis = async () => {
     const res = await fetch(API_URl_FAVORITES);
     const data = await res.json();
 
-    console.log('Favorites');
+    console.log('Favourites');
     console.log(data);
 
     if (res.status !== 200) {
@@ -47,9 +47,36 @@ const loadFavoritesMichis = async () => {
     }else {
         
     }
-   
+};
+
+const saveFavouriteMichis = async () => {
+    const res = await fetch(API_URl_FAVORITES,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            image_id: "e7h"
+
+        }),
+    });
+    const data = await res.json();
+
+
+    console.log('Save');
+    console.log(res);
+
+    if (res.status !== 200) {
+        
+        spanError.innerHTML = `"Hubo un error: Error_${res.status},${data.message}`;
+        spanError.insertAdjacentHTML(
+            "beforeend",
+            `<img src =${API_ERROR_MICHIS}${res.status} id="img-error">`
+        );
+    }
 };
 
 
 loadRandomMichis();
-loadFavoritesMichis();
+loadFavouriteMichis();
+
