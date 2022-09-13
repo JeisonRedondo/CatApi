40259ceed9801c8825a47271ab8d9e1391637b85
JeisonPC <jeida16@yahoo.es>
2022-09-12 19:23:24 -0500

    Terminado fetch propio en Archivo Prueba, Empezando pulido de imagen

--- Ejercicios/prueba.js
+++ Ejercicios/prueba.js
@@ -1,80 +1,116 @@
 const API_CAT = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const URL_API = 'https://api.thecatapi.com/v1/';
+const parametrosGatos = {
+    method:'GET',
+    headers: {
+        'Content-Type': 'application/json',
+        'X-API-KEY':'0e67e603-210b-4a02-9582-778b82a47c62',
+    },
+};
 
-const spanError = document.getElementById('error');
+// let xhr = new XMLHttpRequest();
 
-let xhr = new XMLHttpRequest();
-
-xhr.open('get',API_CAT,true);
-xhr.responseType = 'json';
-xhr.send();
+// xhr.open('get',API_CAT,true);
+// xhr.responseType = 'json';
+// xhr.send();
 
 
-xhr.onload = () => {
-    if (xhr.status !== 200){
-        spanError.innerText = `Hubo un Error = Tipo ${xrh.status}`
-    }else{
+// xhr.onload = () => {
+//     if (xhr.status !== 200){
+//         spanError.innerText = `Hubo un Error = Tipo ${xrh.status}`
+//     }else{
 
-        let responseObj = xhr.response;
-        console.log('Primer intento logrado');
-        console.log('Data:',responseObj);
+//         let responseObj = xhr.response;
+//         console.log('Primer intento logrado');
+//         console.log('Data:',responseObj);
+
+//         const img1 = document.getElementById('img1');
+//         const img2 = document.getElementById('img2');
+
+//         img1.src = responseObj[0].url;
+//         img2.src = responseObj[1].url;
+   
+//     }
+// };
+
+
+
+const promesaFetch = async (url,parametros) => {
+
+
+    let xhr = new XMLHttpRequest();
+    
+    xhr.open('GET',url,true);
+    xhr.responseType = 'json';
+
+    // Object.keys(parametros.headers).forEach( 
+    //     key => xhr.setRequestHeader(key,parametros.headers[key]
+    // ));
+    
+    try {
+        xhr.send();
+
+        xhr.onload = () => {
+            if (xhr.status !== 200) {
+            alert(`Error ${xhr.status}: ${xhr.statusText}`);
+            } else {
+            return (xhr.response);   
+            }
+    }
+    } catch(err) { // en lugar de onerror
+        alert("Solicitud fallida");
+    }
+
+};
+
+async function jeiFetch(url,parametros){
+
+    let xhr = new XMLHttpRequest();
+
+    return new Promise((resolve,reject) => {
+        xhr.open('GET',url,true);
+        xhr.responseType = 'json';
+        Object.keys(parametros.headers).forEach( 
+                key => xhr.setRequestHeader(key,parametros.headers[key]
+            ));
+
+        parametros.hasOwnProperty('body') ?
+            (xhr.send(parametros.body)):
+            xhr.send();
+
+        try {
+            xhr.onload = () => {
+                if (xhr.status !== 200) {
+                    reject(xhr.status)
+                    console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+                } else {
+                    console.log(`Resuelto ${xhr.status}: ${xhr.response}`);
+                    resolve(xhr.response)  
+                }
+        }
+        } catch(err) { // en lugar de onerror
+            alert("Solicitud fallida");
+        }
+
+    })
+}
+
+// ----------------------------------------------------------------------------------- 
+
+// Funnciones de API
+
+async function getGatitosRandom(){
+
+    const API_RANDOM = 'images/search?limit=2';
+
+   let data = await jeiFetch(`${URL_API}${API_RANDOM}`,parametrosGatos);
+    console.log('Data: ',data);
 
         const img1 = document.getElementById('img1');
         const img2 = document.getElementById('img2');
 
-        img1.src = responseObj[0].url;
-        img2.src = responseObj[1].url;
-   
-    }
-};
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+}
 
-
-
-const promesaFetch = new Promise((resolve, reject) => {
-
-    const parametros = {
-        method:'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-KEY':'0e67e603-210b-4a02-9582-778b82a47c62',
-        },
-    }
-
-    let xhr = new XMLHttpRequest();
-    xhr.open(parametros.method,`https://api.thecatapi.com/v1/favourites`,true);
-
-    xhr.responseType = 'json';
-
-    Object.keys(parametros.headers).forEach( 
-        key => xhr.setRequestHeader(key,parametros.headers[key]
-    ));
-
-    
-    parametros.hasOwnProperty('body') ?
-            xhr.send(this.parameters.body):
-            xhr.send();
-
-    xhr.onload = () => {
-        if (xhr.status !== 200){
-            reject(spanError.innerText = `Hubo un Error = Tipo ${xrh.status}`)
-        }else{ 
-            resolve(xhr.response);
-        }
-    };
-})
-
-// -----------------------------------------------------------------------------------
-    const parametros = {
-        method:'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-API-KEY':'0e67e603-210b-4a02-9582-778b82a47c62',
-        },
-    }
-
-    
-    promesaFetch.then(res => {console.log('respuesta Promesa: ',res)})
-                .catch(err => console.log(err))
-
-    
-
-    
+getGatitosRandom()
